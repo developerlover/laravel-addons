070073419247c8adcb06198654f0b613dd674380
Pablo <developerlover83@gmail.com>
2020-07-30 11:40:24 +0200

    first commit

---
+++ src/AdminAuthServiceProvider.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Developerlover\LaravelAddons;
+
+use Illuminate\Support\ServiceProvider;
+
+class AdminAuthServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     */
+    public function register()
+    {
+    }
+
+    /**
+     * Bootstrap services.
+     */
+    public function boot()
+    {
+    }
+}
